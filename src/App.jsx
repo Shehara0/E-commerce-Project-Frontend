6df8e0020d9eb4fe0eb49cf5fd5012030a925e5b
Shehara <sheharasiriwardana21@gmail.com>
2025-10-21 20:23:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
+import AdminPage from "./pages/adminPage";
+import TestPage from "./pages/testPage";
+import { Toaster } from "react-hot-toast";
+import ClientWebPage from "./pages/client/clientPage";
 
 function App() {
- 
-
-  return (
-    <>
-      <div>
-       
-      </div>
-      
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<div className="w-full h-screen flex justify-center items-center bg-primary text-secondary">
+				<Toaster position="top-right" />
+				<Routes path="/">
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/test" element={<TestPage />} />
+          <Route path="/home" element={<HomePage />} />
+					<Route path="/register" element={<RegisterPage />} />
+					<Route path="/admin/*" element={<AdminPage />} />
+					<Route path="/*" element={<ClientWebPage />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
