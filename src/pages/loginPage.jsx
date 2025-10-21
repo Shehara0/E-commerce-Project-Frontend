8bf23ac30208349eecb9a9b774b20e27cee32210
Shehara <sheharasiriwardana21@gmail.com>
2025-10-21 20:03:53 +0530

    error fix login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -10,18 +10,19 @@ export default function LoginPage() {
 
     function login() {
         console.log(email, password);
-        axios.post(import.meta.env.VITE_BACKEND_URL + "/api/users/login", {
+        axios.post(import.meta.env.VITE_BACKEND_URL + "/api/user/login", {
             email: email,
             password: password
         }).then(
             (response) => {
                 console.log(response.data);
                 localStorage.setItem("token", response.data.token);
-                toast.success("Login successful");
 
-                if (response.data.role === "admin") {
+                toast.success("Login successful");
+                
+                if (response.data.user.role == "Admin") {
                     navigate("/admin");
-                } else if (response.data.role === "user") {
+                } else if (response.data.user.role == "Customer") {
                     navigate("/");
                 }
             }
@@ -35,30 +36,40 @@ export default function LoginPage() {
 
     return (
         <div className="w-full h-screen bg-[url(./loginbg.jpg)] bg-cover bg-center flex justify-center items-center">
-            <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] relative gap-[20px] text-white flex flex-col items-center justify-center">
+            <div className="w-[500px] h-[450px] backdrop-blur-sm shadow-2xl rounded-[30px] relative gap-[20px] text-white flex flex-col items-center justify-center">
                 <h1 className="absolute top-[20px] text-2xl font-bold text-center my-5">Login</h1>
+                
                 <div className="w-[350px] flex flex-col">
                     <span className="text-lg">Email</span>
-                    <input
-                        onChange={(e) => setEmail(e.target.value)}
-                        type="text"
-                        className="w-[350px] h-[40px] border border-white rounded-xl"
+                    <input 
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        type="text" 
+                        className="w-[350px] h-[40px] border border-white rounded-xl px-3 bg-transparent text-white placeholder-gray-300"
+                        placeholder="Enter your email"
                     />
                 </div>
+
                 <div className="w-[350px] flex flex-col">
                     <span className="text-lg">Password</span>
-                    <input
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password"
-                        className="w-[350px] h-[40px] border border-white rounded-xl"
+                    <input 
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }} 
+                        type="password" 
+                        className="w-[350px] h-[40px] border border-white rounded-xl px-3 bg-transparent text-white placeholder-gray-300"
+                        placeholder="Enter your password"
                     />
                 </div>
-                <button
-                    onClick={login}
+
+                <button 
+                    onClick={login} 
                     className="w-[350px] h-[40px] bg-blue-500 rounded-xl text-white text-lg mt-5 hover:bg-blue-600 transition-all duration-300"
                 >
                     Login
                 </button>
+
                 <p>
                     Don't have an account? <Link to="/register" className="text-blue-500">Sign up</Link> from here
                 </p>
